f568c3a2b10bfd952e0e1fc46dea8f9c74ece51a
Mona Lisa <limn64@yahoo.com>
2018-05-02 21:00:17 -0700

    add all

--- controllers/user.js
+++ controllers/user.js
@@ -14,8 +14,20 @@ userController.list = function(req, res) {
 };
 
 userController.edit = function(req, res) {
+  const userId = req.params.id;
+
+  if (userId) {
+    User.findOne({ _id: userId }).exec(function(err, user) {
+      if (err) {
+        console.log("Error:", err);
+      } else {
+        res.render("users/edit", { user });
+      }
+    });
+  } else {
     res.render("users/create");
-  };
+  }
+};
   
   userController.create = function(req, res) {
     var user = new User(req.body);
@@ -30,6 +42,43 @@ userController.edit = function(req, res) {
         res.json(newUser);
       }
     });
+  };
+
+  userController.update = function(req, res) {
+    const updatedUser = req.body;
+    User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          name: updatedUser.name,
+          address: updatedUser.address,
+          position: updatedUser.position,
+          salary: updatedUser.salary
+        }
+      },
+      { new: true }, // return updated
+      function(err, user) {
+        console.log("in update callback");
+        if (err) {
+          console.log("failed to update", err);
+        }
+        res.json(user);
+      }
+    );
+  };
+
+  userController.delete = function(req, res) {
+    const uid = req.params.id;
+    User.remove({ _id: uid }, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`User ${uid} deleted!`);
+      }
+    });
+  
+    // the server should respond with data to fire jquery callback
+    res.json({});
   };
 
 module.exports = userController;
--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,10 @@ router.post("/user", user.create);
 // Edit or create a user
 router.get("/user/:id?", user.edit);
 
+router.put("/user/:id", user.update);
+
+// Delete user
+router.delete("/user/:id", user.delete);
+
 module.exports = router;
 
